03100a3a47f16eac90ca3c7cb7f000ba5187592a
Andrew <Andrey18651@gmail.com>
2021-09-08 23:38:41 +0300

    update excel: add check_upload_fields and add_success_point_to_applicant functions

--- utils/excel.py
+++ utils/excel.py
@@ -12,3 +12,29 @@ def convert_excel_to_list(filename: str) -> list:
         return df
     except Exception:
         raise ExcelFileNotFoundException
+
+
+def check_upload_field(filename: str) -> bool:
+    """
+    Проверка, существует ли поле upload в Excel файле
+    """
+    try:
+        df = read_excel(f'{filename}')
+        if 'upload' in df.columns:
+            return True
+        df.insert(5, 'upload', 0)
+        df.to_excel(f'{filename}', index=False)
+        return True
+    except Exception:
+        raise ExcelFileNotFoundException
+
+
+def add_success_point_to_applicant(filename: str, applicant: str) -> bool:
+    """
+    Добавление в колонку upload статуса обработки кандидата
+    """
+    df = read_excel(f'{filename}')
+    applicant_index = df.index[df['ФИО'] == applicant][0]
+    df.at[applicant_index, 'upload'] = 1
+    df.to_excel(f'{filename}', index=False)
+    return True
