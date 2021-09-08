b1bcac488e877e38e12f58a3ed660491eefd40dc
Andrew <Andrey18651@gmail.com>
2021-09-08 23:42:12 +0300

    update exceptions: add extra exceptions

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -7,7 +7,47 @@ class ExcelFileNotFoundException(HuntFlowBaseException):
     Обработка ошибки отсутствия Excel файла
     """
 
-    def __init__(self, message='Excel-файл не найден, убедитесь, что он находится в корне проекта'):
+    def __init__(
+            self,
+            message=('Excel-файл не найден, '
+                     'убедитесь, что он находится в корне '
+                     'проекта в папке Resumes')):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class WrongVacancyNameException(HuntFlowBaseException):
+    """
+    Обработка ошибки неверного названия вакансии в таблице
+    """
+
+    def __init__(
+            self,
+            message=(
+                    'Передано неверное название вакансии, '
+                    'убедитесь, что в табилце указано такое '
+                    'же название вакансии, как на сайте')):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class WrongStatusNameException(HuntFlowBaseException):
+    """
+    Обработка ошибки неверного названия статуса в таблице
+    """
+
+    def __init__(
+            self,
+            message=(
+                    'Передано неверное название статуса, '
+                    'убедитесь, что в табилце указан такое '
+                    'же статус, как на сайте')):
         self.message = message
         super().__init__(self.message)
 
